fbcdcc572ef23992e27149c5bb1edd0d3d782e25
jukegbu1 <jukegbu1@gmail.com>
2019-09-10 10:39:17 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Cost" + "\t" + "\t" + "Quantity" + "\t" + "\t" + "Total");
+        System.out.println("$1,000.00" + "\t" + "4" + "\t" + "\t" + "\t" + "\t" + "$4,000.00");
+        System.out.println("$50.00" + "\t" + "\t" + "8" + "\t" + "\t" + "\t" + "\t" + "$400.00");
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("_________________________________________");
+        System.out.println("Total:" + "\t" + "\t" + "12" + "\t" + "\t" + "\t" + "\t" + "$4,400.00");
+
+
+    }
+}
